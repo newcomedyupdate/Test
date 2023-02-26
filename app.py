29f468b189582bb2b055dc9345eca1a568856c04
newcomedyupdate <126194246+newcomedyupdate@users.noreply.github.com>
2023-02-27 01:36:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,9 @@ number=st.slider("Hey pick number",0,10)
 
 st.write("Square of number",number**2)
 import pandas as pd
+st.write("Hey")
+file=st.file_uploader("Please upload CSV here")
+
+df=pd.read_csv("file")
+
+st.write(df)
